1b8476048348a8f607592b7c31441b6ba6e657de
rasokan <rajmohan.asokan@gmail.com>
2015-08-26 10:28:07 -0700

    Quick Sort routine included

--- Algorithms_Implementations/Algorithms_Implementations.cpp
+++ Algorithms_Implementations/Algorithms_Implementations.cpp
@@ -4,17 +4,18 @@
 #include <vector>
 class Algorithms{
 private:
-	int *x, *y; int begin, end, midpoint;
+	int *x; int begin, end, midpoint;
 public:
-	int* merge_sort(int* x,  int begin, int end);
-	int* merge(int* x,  int begin, int middle, int end);
-	int* heap_sort(int* x, int length);
-	int* build_max_heap(int* x, int length);
-	int* max_heapify(int* x, int node, int length);
-
+	void merge_sort(int* x,  int begin, int end);
+	void merge(int* x,  int begin, int middle, int end);
+	void heap_sort(int* x, int length);
+	void build_max_heap(int* x, int length);
+	void max_heapify(int* x, int node, int length);
+	void quick_sort(int* x, int begin, int end);
+	int quick_partition(int*x, int begin, int end);
 };
 
-int* Algorithms::merge_sort(int* x,int begin, int end){
+void Algorithms::merge_sort(int* x,int begin, int end){
 	int middle;
 	if(begin < end){
 		middle = (begin+end)/2;
@@ -23,9 +24,9 @@ int* Algorithms::merge_sort(int* x,int begin, int end){
 		Algorithms::merge(x, begin, middle, end);
 	}
 
-	return x;
+	
 }
-int* Algorithms::merge(int* x,int begin, int middle, int end){
+void Algorithms::merge(int* x,int begin, int middle, int end){
 	int *y; int i = begin, k = begin, j = middle+1;
 	y = new int[50];
 	
@@ -49,10 +50,10 @@ int* Algorithms::merge(int* x,int begin, int middle, int end){
 	}
 	for(i = begin;i < k; i++){ 	x[i] = y[i]; }
 
-	return x;
+	
 }
 
-int* Algorithms::heap_sort(int* x, int length){
+void Algorithms::heap_sort(int* x, int length){
 	
 	Algorithms::build_max_heap(x, length);
 	for(int i = length; i>0; i--){
@@ -63,18 +64,18 @@ int* Algorithms::heap_sort(int* x, int length){
 		length--;
 		Algorithms::max_heapify(x, 0, length);
 	}
-	return x;
+	
 }
 
-int* Algorithms::build_max_heap(int* x, int length){
+void Algorithms::build_max_heap(int* x, int length){
 	int heap_size = length;
 	for(int i = length/2; i >= 0; i--){
 		Algorithms::max_heapify(x, i, length);
 	}
-	return x;
+	
 }
 
-int* Algorithms::max_heapify(int* x, int node, int length){
+void Algorithms::max_heapify(int* x, int node, int length){
 	int left, right;
 	if(node!=0){
 		 left = 2*node;  right = 2*node+1;
@@ -93,9 +94,44 @@ int* Algorithms::max_heapify(int* x, int node, int length){
 		x[largest] = temp;
 		Algorithms::max_heapify(x, largest, length);
 	}
-	return x;
+	
 }
 
+void Algorithms::quick_sort(int* x, int begin, int end){
+	if(begin < end){
+		int partition = Algorithms::quick_partition(x, begin, end);
+		//std::cout<<"Partition: "<< partition<< std::endl;
+		Algorithms::quick_sort(x, begin, partition-1);
+		Algorithms::quick_sort(x, partition+1, end);
+	}
+	
+}
+
+int Algorithms::quick_partition(int* x, int begin, int end){
+		int pivot = rand()%((end-begin)+1);
+		//std::cout<<"Pivot: "<<pivot;
+		int temp;
+		temp = x[(pivot+begin)];
+		x[(pivot+begin)] = x[end];
+		x[end] = temp;
+		pivot = x[end];
+		std::cout<<"Pivot: "<<pivot;
+		int i = begin-1;
+		for(int j = begin; j < end; j++){
+			if(x[j] <= pivot){
+				i++;
+				temp = x[i];
+				x[i] = x[j];
+				x[j] = temp;
+			}
+		}
+		int partition= ++i;
+		temp = x[partition];
+		x[partition] = x[end];
+		x[end] = temp;
+		std::cout<<"Partition: "<< partition<< std::endl;
+		return partition;
+}
 int main(int argc,char** argv){
 	int num;
 	std::cout<< "Enter the number of elements: "<< std::endl;
@@ -108,10 +144,12 @@ int main(int argc,char** argv){
 	std::cout<< "Enter the number: "<< std::endl;
 	std::cin>> ar[iter];
 	}
-	
+	//std::cout<< rand()%num<< std::endl;
 	Algorithms n;
-	//n.merge_sort(ar,0,num-1); //Invokes Merge Sort
-	n.heap_sort(ar,num-1); //Invokes Heap Sort
+	n.merge_sort(ar,0,num-1); //Invokes Merge Sort
+	//n.heap_sort(ar,num-1); //Invokes Heap Sort
+	//n.quick_sort(ar, 0, num-1);//Invokes Quick Sort
+	std::cout<< "Sorted Order: "<< std::endl;
 	for(int iter = 0;iter<num; iter++){
 		std::cout<< ar[iter]<< std::endl;	
 	}
